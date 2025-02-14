f475e198d7106675a26915b7f97e837ba9de2cae
Oleksandr Nykytenko <alexvwan@gmail.com>
2025-02-14 23:30:31 +0200

    update version to 1.0.13

--- src/example_plugin.ts
+++ src/example_plugin.ts
@@ -3,7 +3,7 @@ import {FDO_SDK, FDOInterface, PluginMetadata} from '@anikitenko/fdo-sdk';
 class MyPlugin extends FDO_SDK implements FDOInterface {
     private readonly _metadata: PluginMetadata = {
         name: "MyPlugin",
-        version: "1.0.12",
+        version: "1.0.13",
         author: "AleXvWaN",
         description: "A sample FDO plugin",
         icon: "COG",
